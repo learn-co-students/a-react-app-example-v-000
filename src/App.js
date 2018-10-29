e39809a38cf2b7140aa1c2a4cf769a4e8d6cf237
github username <github email address>
2018-10-29 16:06:28 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import TestComponent from './TestComponent'
 
 // Add your code own within the return statement
 
+
+//notes
+// contains three child elements, <header>, <p> and <ExampleComponent />
+
 class App extends Component {
   render() {
 
     return (
       <div className="App">
         <header className="App-header">
-          {moment().format('MMMM Do YYYY, hh:mm:ss a')}
+          <h1>Now</h1>
         </header>
         <p className="App-intro">
           In React apps, we write JSX - it looks like HTML, and uses a lot HTML syntax.
@@ -19,7 +23,7 @@ class App extends Component {
           allows us to add in components, which are separate, self-contained chunks of of JSX.
         </p>
         <ExampleComponent />
-
+        <TestComponent />
       </div>
     );
   }
