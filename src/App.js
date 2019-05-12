f508242084d0480cf3c7faabb0db4b8c771425c8
Winston Tokuhisa <TokuhisaWinston@Outlook.com>
2019-05-12 05:47:07 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -11,15 +11,26 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {moment().format('MMMM Do YYYY, hh:mm:ss a')}
+          {/*{moment().format('MMMM Do YYYY, hh:mm:ss a')}*/}
+          {/* Replace the contents of the header element so that, instead of a
+            time, it just says 'Now' */}
+          Now
         </header>
+
         <p className="App-intro">
-          In React apps, we write JSX - it looks like HTML, and uses a lot HTML syntax.
-          JSX lets us include JavaScript functions right along with the HTML, and also
-          allows us to add in components, which are separate, self-contained chunks of of JSX.
+          In React apps, we write JSX - it looks like HTML, and uses a lot HTML
+          syntax. JSX lets us include JavaScript functions right along with the
+          HTML, and also allows us to add in components, which are separate,
+          self-contained chunks of of JSX.
         </p>
+
+        {/* Make sure to include <ExampleComponent /> (if you have removed it)
+          */}
         <ExampleComponent />
 
+        {/* Add in a new component, <TestComponent />, just below
+          <ExampleComponent /> */}
+        <TestComponent />
       </div>
     );
   }
