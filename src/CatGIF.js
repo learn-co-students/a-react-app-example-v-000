68ae47fede589bec57df9f00a82aee360763f7f1
github username <github email address>
2019-07-21 13:45:28 -0700

    Add file

---
+++ src/CatGIF.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class CatGIF extends Component {
+  render () {
+    return (
+      <div>
+        <img src='https://media1.giphy.com/media/13CoXDiaCcCoyk/giphy.gif'></img>
+      </div>
+    )
+  }
+}
