1162094eb192fa747f4d0779be2f3c37b068e03a
jamieberrier <berrier.jamie@gmail.com>
2020-08-21 11:32:55 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+// App.js is pulling in specific content from these two packages! 
+// They are being imported from the node_modules folder.
 import React, { Component } from 'react';
 import moment from 'moment';
+// App.js is importing files in the same directory
+// // // This is the syntax to import something that is the default export of another file:
 import ExampleComponent from './ExampleComponent'
 import TestComponent from './TestComponent'
 
 // Add your code own within the return statement
-
+// all of the visible content of our app is contained within this App class.
 class App extends Component {
   render() {
 
     return (
       <div className="App">
         <header className="App-header">
-          {moment().format('MMMM Do YYYY, hh:mm:ss a')}
+          Now
+          {/*{moment().format('MMMM Do YYYY, hh:mm:ss a')}*/}
         </header>
         <p className="App-intro">
           In React apps, we write JSX - it looks like HTML, and uses a lot of HTML syntax.
@@ -19,10 +24,12 @@ class App extends Component {
           allows us to add in components, which are separate, self-contained chunks of JSX.
         </p>
         <ExampleComponent />
+        <TestComponent />
 
       </div>
     );
   }
 }
-
+// allowing other files to import things from the App.js file
+// denotes that our App class is the main thing we want to export from our App.js file.
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// This is the syntax to import something that is the default export of another file:
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
