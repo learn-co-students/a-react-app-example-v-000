c929915bb013551f56847ba43624edcfc2a4abad
RonSala <ron.sala@gmail.com>
2020-09-13 11:31:51 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* {moment().format('MMMM Do YYYY, hh:mm:ss a')} */}
           Now
         </header>
         <p className="App-intro">
